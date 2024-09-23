ea0929bfd192c1750822c96cfc888e93d08665cf
OrlandoRome <Orlando@certiverse.com>
2024-09-22 23:52:47 -0400

    se seteo mi nombre

--- app/src/main/java/com/example/clase1/MainActivity.kt
+++ app/src/main/java/com/example/clase1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Clase1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Orlando Romero",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
